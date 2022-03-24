1c40f3c644c6f92c3759db3cb89a29de36b3aba8
Katarina Brayner <katarinabrayner@gmail.com>
2022-03-24 12:58:45 -0300

    adiciona diretorio para task javascript

---
+++ TaskJavaScript/Script.js
@@ -0,0 +1,63 @@
+//Crie um script que leia o nome da pessoa e mostre o nome dela em um alert() e no console 
+
+/*
+let nome = prompt("Escreva seu nome");
+
+alert("Seu nome é " +nome);
+console.log(nome);
+*/
+
+
+
+//Crie um script que leia do usuário dois números e mostre a soma desses dois números.
+
+/*
+let num1 = parseInt(prompt("Digite um numero"));
+let num2 = parseInt(prompt("Digite outro numero"));
+
+let soma = num1 + num2;
+
+alert("O resultado da sua soma é " + soma)
+console.log(soma);
+*/
+
+
+//Crie um script que leia duas notas do usuário e calcule a média do aluno.
+
+/*
+let nota1 = parseInt(prompt("Digite sua primeira nota"));
+let nota2 = parseInt(prompt("Digite a sua segunda nota"));
+
+let media = (nota1 + nota2) / 2;
+
+alert("A sua média é " + media);
+console.log(media);
+*/
+
+
+//Crie um programa leia o ano em que o usuário nasceu e diga a idade atual dele.
+
+/*
+let anoNascimento = parseInt(prompt("Digite o ano do seu nascimento"));
+let hoje = new Date();
+let anoAtual = hoje.getFullYear();
+let idade = anoAtual - anoNascimento;
+
+alert("Sua idade é " + idade);
+console.log(idade);
+*/
+
+
+// Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+
+/*
+let salarioHora = parseInt(prompt("Digite o seu salário por hora"));
+let horasTrabalhadas = parseInt(prompt("Digite a horas trabalhadas no mês"));
+
+let salario = salarioHora * horasTrabalhadas
+
+alert("Seu salário do mês foi " + salario);
+console.log(salario);
+*/
+
